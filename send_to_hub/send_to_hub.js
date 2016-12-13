6fe5c08d90f573ebd9cc872d5bc2d87e24e31ed6
unicorns <you@exam.com>
2016-12-14 00:59:30 +0100

    sending data from database to hub

--- send_to_hub/send_to_hub.js
+++ send_to_hub/send_to_hub.js
@@ -15,11 +15,17 @@ var Message = require('azure-iot-device').Message;
 // String containing Hostname, Device Id & Device Key in the following formats:
 //  "HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
 //var connectionString = "ID=power PrimaryKey=YQajcX2Ak8J5DOROXfPq4nHFJHqUZeI982gE8gVMc0M= SecondaryKey=Eh7qWBQdiMaz8MtDTn38UDiC9wQqT7Vm6udTWf+ZWIg=";
-var connectionString = "HostName=power.azure-devices.net;DeviceId=power;SharedAccessKey=YQajcX2Ak8J5DOROXfPq4nHFJHqUZeI982gE8gVMc0M=";
+var connectionString = "HostName=power.azure-devices.net;DeviceId=pawel;SharedAccessKey=E9YTR1A4XcaiHJcH01Dn3aRguY7i0jJHggZOzVdNd6Q=;"
 //  "HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
 // fromConnectionString must specify a transport constructor, coming from any transport package.
 var client = Client.fromConnectionString(connectionString, Protocol);
-
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'garden'
+});
 var connectCallback = function (err) {
   if (err) {
     console.error('Could not connect: ' + err.message);
@@ -33,21 +39,29 @@ var connectCallback = function (err) {
     });*/
 
     // Create a message and send it to the IoT Hub every second
-    var sendInterval = setInterval(function () {
+    var sendInterval = setIterval( function () {
       var windSpeed = 10 + (Math.random() * 4); // range: [10, 14]
       var data = JSON.stringify({ deviceId: 'myFirstDevice', windSpeed: windSpeed });
-      var message = new Message(data);
+connection.query('SELECT * from sensors_data', function(error, results, fields) {
+  data=JSON.stringify({costam: results[0].temperature});
+
+  var message = new Message(data);
       console.log('Sending message: ' + message.getData());
       client.sendEvent(message, printResultFor('send'));
-    }, 6000);
+    });
+console.log(data);
+    },15000);
+ 
+
 
     client.on('error', function (err) {
       console.error(err.message);
     });
 
     client.on('disconnect', function () {
-      clearInterval(sendInterval);
+      //clearInterval(sendInterval);
       client.removeAllListeners();
+	connection.connect();
       client.open(connectCallback);
     });
   }
